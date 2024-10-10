ee315760de7357b39f330d63fea5cce6ad385280
Sean Murphy <sean@iamseanmurphy.com>
2024-10-10 21:11:25 +0000

    1.1.1

---
+++ neve-fse/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve-fse',
+        'pretty_version' => 'v1.1.1',
+        'version' => '1.1.1.0',
+        'reference' => '0770fae18afb29760d4d8bb098cd23fc4c0973cd',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve-fse' => array(
+            'pretty_version' => 'v1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => '0770fae18afb29760d4d8bb098cd23fc4c0973cd',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.29',
+            'version' => '3.3.29.0',
+            'reference' => '4d63bc81ec0357c8675dac1e35511fd81104605d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
