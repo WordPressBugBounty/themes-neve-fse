dddfd3f2cd6b73c21caa322812478961f26f6bad
Sean Murphy <sean@iamseanmurphy.com>
2025-04-17 17:17:16 +0000

    1.1.3

--- neve-fse/vendor/composer/installed.php
+++ neve-fse/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/neve-fse',
-        'pretty_version' => 'v1.1.2',
-        'version' => '1.1.2.0',
-        'reference' => '5c5c0c3058c600a9a611a6355301b58982bfb591',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9e91d6565357e5ca0b3428669a795f857d21b6d0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/neve-fse' => array(
-            'pretty_version' => 'v1.1.2',
-            'version' => '1.1.2.0',
-            'reference' => '5c5c0c3058c600a9a611a6355301b58982bfb591',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9e91d6565357e5ca0b3428669a795f857d21b6d0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.37',
-            'version' => '3.3.37.0',
-            'reference' => '73e9010fb4e30ad8835f00a0df802733f98c4094',
+            'pretty_version' => '3.3.44',
+            'version' => '3.3.44.0',
+            'reference' => 'fed444b52ebf1f689ec2434df177926bf8f238c4',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
